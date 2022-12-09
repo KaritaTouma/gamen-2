5bcc3997910bee5aeba7a90e07f1a19cd02d1ecc
Karita <tejc190103@gmail.com>
2022-12-09 12:46:04 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -83,7 +83,7 @@ function createData(name, message) {
 }
 
 const rows = [
-  createData('Aさん', 'あああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ'),
+  createData('Aさん', 'ああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ'),
   createData('Bさん', 'I like pancakes.'),
   createData('Cさん', 'ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc'),
   createData('Dさん', ),
