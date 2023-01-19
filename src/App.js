705e702d4707fbe81912f6aed4e5f42ef878791b
Karita <tejc190103@gmail.com>
2023-01-19 15:32:20 +0900

    ボタンデザイン更新

--- src/App.js
+++ src/App.js
@@ -69,6 +69,7 @@ function TablePaginationActions(props) {
       >
         {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
       </IconButton>
+    
       <button1>戻る</button1>
     </Box>
     
